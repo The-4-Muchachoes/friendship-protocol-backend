b4f3e1f3cc2b3f191701ced5366ffcb727cc7297
Lars <larsriel@gmail.com>
2021-12-03 08:57:42 +0100

    first commit

---
+++ src/main/java/com/muchachos/friendshipprotocol/FriendshipProtocolApplication.java
@@ -0,0 +1,13 @@
+package com.muchachos.friendshipprotocol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FriendshipProtocolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FriendshipProtocolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/muchachos/friendshipprotocol/FriendshipProtocolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.muchachos.friendshipprotocol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FriendshipProtocolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
